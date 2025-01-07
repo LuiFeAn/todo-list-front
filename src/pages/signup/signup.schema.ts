9cfd2c9f9cf2cc21eff14980e3e409726eb0db8e
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-07 14:55:27 -0300

    feat: add signup schema and controller; simplify login password validation

---
+++ src/pages/signup/signup.schema.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  email: z
+    .string()
+    .email("O email informado é inválido.")
+    .nonempty("O email é obrigatório."),
+  password: z
+    .string()
+    .min(8, "A senha deve ter no mínimo 8 caracteres.")
+    .nonempty("A senha é obrigatória.")
+    .refine((value) => /[A-Z]/.test(value), {
+      message: "A senha deve conter pelo menos uma letra maiúscula.",
+    })
+    .refine((value) => /[0-9]/.test(value), {
+      message: "A senha deve conter pelo menos um número.",
+    })
+    .refine((value) => /[!@#$%^&*(),.?":{}|<>]/.test(value), {
+      message: "A senha deve conter pelo menos um símbolo.",
+    }),
+});
+
+export type SignUpSchemaType = z.infer<typeof signUpSchema>;
