7091a25c3b2e0383598c75cda4805c3322dc6a54
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-07 19:36:11 -0300

    feat: add visual indication for completed todos in card component

--- src/pages/todo/components/card_area/components/card/index.tsx
+++ src/pages/todo/components/card_area/components/card/index.tsx
@@ -6,6 +6,7 @@ interface Props {
   todo: ITodo;
   refetch(): Promise<void>;
 }
+
 export default function Card({ todo, refetch }: Props) {
   const { handleCardColor, handleCardDel, makeDone } = useCardController({
     refetch,
@@ -17,6 +18,14 @@ export default function Card({ todo, refetch }: Props) {
         todo.priority
       )} lg:w-[400px] pl-4 lg:h-[400px] rounded-lg relative cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-lg bg-yellow-200 rotate-1 shadow-md shadow-gray-400 border border-gray-300`}
     >
+      {todo.done && (
+        <div className="absolute inset-0 flex items-center justify-center bg-green-500 bg-opacity-50 rounded-lg z-10">
+          <p className="text-white text-2xl font-bold animate-pulse">
+            ✔ Concluída!
+          </p>
+        </div>
+      )}
+
       <h1 className="text-2xl text-start mt-5 font-bold">{todo.title}</h1>
       <p
         className={`mt-3 font-bold pl-2 rounded-l-lg text-white ${handleCardColor(
@@ -28,6 +37,7 @@ export default function Card({ todo, refetch }: Props) {
       <div>
         <p className="pt-10 break-words">{todo.description}</p>
       </div>
+
       <div className="flex gap-2 absolute bottom-10 right-5">
         {!todo.done && (
           <Check
