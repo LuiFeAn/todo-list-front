4e8f673bbfa9a3b90b5654ec1d2cff10e3d2d410
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-07 18:26:46 -0300

    feat: update Card component styles with new background color and improved priority display

--- src/pages/todo/components/card/index.tsx
+++ src/pages/todo/components/card/index.tsx
@@ -5,7 +5,6 @@ import { ITodo } from "../../todo.service.interfaces";
 interface Props {
   todo: ITodo;
 }
-
 export default function Card({ todo }: Props) {
   const { handleCardColor } = useCardController();
 
@@ -13,10 +12,12 @@ export default function Card({ todo }: Props) {
     <div
       className={`${handleCardColor(
         todo.priority
-      )} lg:w-[400px] pl-4 lg:h-[400px] rounded-lg relative cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-lg`}
+      )} lg:w-[400px] pl-4 lg:h-[400px] rounded-lg relative cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-lg bg-yellow-200 rotate-1 shadow-md shadow-gray-400 border border-gray-300`}
     >
       <h1 className="text-2xl text-start mt-5 font-bold">{todo.title}</h1>
-      <p className="mt-3 font-bold">{todo.priority}</p>
+      <p className={`mt-3 font-bold pl-2 rounded-l-lg text-white ${handleCardColor(todo.priority)}`}>
+        {todo.priority}
+      </p>
       <div>
         <p className="pt-10 break-words">{todo.description}</p>
       </div>
