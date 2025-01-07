96f3c8b0f9a636cf35458230524091c80d49af81
LuiFeAn <luisfernandogvv@gmail.com>
2025-01-07 15:36:41 -0300

    feat: integrate toast notifications for signup success and error handling

--- src/pages/signup/signup.controller.ts
+++ src/pages/signup/signup.controller.ts
@@ -2,19 +2,36 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { signUpSchema, SignUpSchemaType } from "./signup.schema";
 import useSignupService from "./signup.service";
+import { AxiosError } from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export default function useSignUpController() {
   const { register: registerUser } = useSignupService();
 
+  const Nav = useNavigate();
+
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<SignUpSchemaType>({
     resolver: zodResolver(signUpSchema),
   });
 
-  const onSubmit = (data: SignUpSchemaType) => registerUser(data);
+  const onSubmit = async (data: SignUpSchemaType) => {
+    try {
+      await registerUser(data);
+      reset();
+      Nav("/");
+      toast.success("Registrado com Sucesso!");
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        toast.error(err.response?.data.message);
+      }
+    }
+  };
 
   return {
     register,
